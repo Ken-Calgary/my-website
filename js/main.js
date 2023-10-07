0e23e64e26163f03e6086fa5dd18b72de551deaa
Soul <ken.liu1@ucalgary.ca>
2023-10-07 15:58:52 -0600

    Huge update
    - Added darktheme toggler
    - Updated welcome page to include moving button, and background image
    - Added socials onto the main page
    - Decided to remove navigation bar and make it all in one page
    - Added content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 function transition() {
     document.getElementById("main-container").scrollIntoView({behavior: "smooth"});
 }
+
+function toggleTheme() {
+    var main = document.getElementById("main-container");
+    var themeButton = document.getElementById("theme-toggler");
+    var github = document.getElementById("github-icon");
+
+    if (main.classList.contains("dark-theme")) {
+        main.classList.remove("dark-theme");
+        themeButton.classList.remove("white-font");
+        github.classList.remove("inverse");
+    } else {
+        main.classList.add("dark-theme");
+        themeButton.classList.add("white-font");
+        github.classList.add("inverse");
+    }
+}
