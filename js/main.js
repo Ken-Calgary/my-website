c594f98fae2cd3a1ce82dd4d7d374426168dfb02
Soul <ken.liu1@ucalgary.ca>
2023-10-07 01:42:23 -0600

    Massive update to overall website
    - Updated html to lessen css load, by adding a few classes
    - html now includes some social (aim to fix formatting or add more pages)
    - still debating on how to auto scroll for javascript part
    - uploading images for socials, pfp and favicon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 function transition() {
-    var element = document.getElementById("welcome-container");
-    element.classList.add("transition");
-    
-    element
+    document.getElementById("main-container").scrollIntoView({behavior: "smooth"});
 }
