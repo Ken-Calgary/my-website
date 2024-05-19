d281d5bd75250064ff466cfd7a614fa45922a43f
Soul <ken.liu1@ucalgary.ca>
2024-05-19 01:58:30 -0600

    Revamping everything

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
 # My Personal Website
-A personal website for myself, enjoyed my time making a website while chilling for the weeks to come.
-If you have any questions, concerns or what not, you can email me at: kenyjliu@gmail.com, see everyone around :). 
-
-P.S. Need to fix it for mobile view, otherviews and what not, will work on that on a later date.
-
-## Credits:
-
-Background Image (Mady by: SeerLight): 
-[DevianArt](https://www.deviantart.com/seerlight), 
-[Instagram](https://www.instagram.com/seerlight/), 
-[Twitter/X](https://twitter.com/seerlight?lang=en), 
-[Pinterest](https://www.pinterest.ca/seerlight/),
-[YouTube](https://www.youtube.com/@seerlights/featured)
-
-OST (Mady by: My friend Danrel :^)):
-[Github](https://github.com/Naki-Cloud)
-[LinkedIn](https://www.linkedin.com/in/danrel-lu-b51093273/)
+A personal website for myself to describe myself as a programmer.
--- js/main.js
+++ js/main.js
@@ -1,31 +1,6 @@
-var ost = new Audio("./music/ost.mp3");
-ost.loop = true;
-var muted = true;
+// Script to load bubbles
 
-function transition() {
-    document.getElementById("main-container").scrollIntoView({behavior: "smooth"});
-}
-
-function toggleTheme() {
-    var main = document.getElementById("main-container");
-    var themeButton = document.getElementById("theme-toggler");
-    var github = document.getElementById("github-icon");
-
-    main.classList.toggle("dark-theme");
-    themeButton.classList.toggle("white-font");
-    github.classList.toggle("inverse");
-}
-
-function toggleSound() {
-    let img = document.getElementById("volume-button");
-
-    if (muted) {
-        img.src = "./img/unmuted.png";
-        ost.play();
-        muted = false;
-    } else {
-        img.src = "./img/muted.png";
-        ost.pause();
-        muted = true;
-    }
+for (i = 0; i <= 38; i++) {
+    let randomNum = Math.random() * 30;
+    document.getElementsByClassName("bubbles")[0].innerHTML += `<span class = "center-text" style="--i:${randomNum};"></span>\n`;
 }
