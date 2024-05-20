7c917c6a7b65e211788de1aaf7385bcdd638961f
Soul <ken.liu1@ucalgary.ca>
2024-05-20 15:17:34 -0600

    Added functional buttons

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function scrollToTarget(elementPosition) {
+    const navOffset = 48; // Navbar offset
+    let offsetPosition = elementPosition + window.scrollY - navOffset;
+    console.log('hi');
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
+
+function homeClicked() {
+    window.scrollTo({
+        top: 48,
+        behavior: "smooth"
+    });
+}
+
+function aboutClicked() {
+    let elementPosition = document.getElementById("about").getBoundingClientRect().top;
+    scrollToTarget(elementPosition)
+}
+
+function skillsClicked() {
+    let elementPosition = document.getElementById("skills").getBoundingClientRect().top;
+    scrollToTarget(elementPosition)
+}
+
+function projectsClicked() {
+    let elementPosition = document.getElementById("projects").getBoundingClientRect().top;
+    scrollToTarget(elementPosition)
+}
